70fb13b59636eefda6d69244ebae6ffbd32e9feb
mianke <3089666858@qq.com>
2025-12-07 19:46:15 +0800

    改kill.sh为批量关闭

--- src/kill.sh
+++ src/kill.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-screen -S onebot -X kill
-screen -S nyxbot -X kill
-echo "机器人服务已关闭"
+screen -ls | grep "nyxbot" | awk '{print $1}' | xargs -I {} screen -S {} -X kill &> /dev/null
+echo "OneBot已关闭"
+screen -ls | grep "onebot" | awk '{print $1}' | xargs -I {} screen -S {} -X kill &> /dev/null
+echo "NyxBot已关闭"
