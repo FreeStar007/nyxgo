ee17cb2c9054c493a020a28f502edc46e0112fff
FreeStar007 <3089666858@qq.com>
2026-03-10 00:06:54 +0800

    优化kill.sh与reset.sh两个脚本的错误处理与信息提示

--- src/kill.sh
+++ src/kill.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-screen -ls | grep "nyxbot" | awk '{print $1}' | xargs -I {} screen -S {} -X kill &> /dev/null
-echo "OneBot已关闭"
-screen -ls | grep "onebot" | awk '{print $1}' | xargs -I {} screen -S {} -X kill &> /dev/null
-echo "NyxBot已关闭"
+if (screen -ls | grep "nyxbot" | awk '{print $1}' | xargs -I {} screen -S {} -X kill) &> /dev/null; then
+    echo "OneBot已关闭"
+else
+    echo "OneBot关闭失败，有可能没开"
+fi
+if (screen -ls | grep "onebot" | awk '{print $1}' | xargs -I {} screen -S {} -X kill) &> /dev/null; then
+    echo "NyxBot已关闭"
+else
+    echo "NyxBot关闭失败，有可能没开"
+fi
+echo "若screen只有以上两个服务进程，则可通过“pkill screen”命令来一键停止以上两个服务"
--- src/reset.sh
+++ src/reset.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sudo rm -rf /usr/local/nyxgo_venv
-echo "环境已重置，可重新运行start.sh"
+if sudo rm -rf /usr/local/nyxgo_venv &> /dev/null; then
+    echo "环境已重置，可重新运行start.sh"
+else
+    echo "环境重置失败，重试一下吧"
+fi
